471710648ae1517e3722efcfb730ab3479acacd8
someuser00 <47272066+someuser00@users.noreply.github.com>
2023-08-23 10:57:27 +0530

    Update reply_factory.py
    Completed task

--- core/reply_factory.py
+++ core/reply_factory.py
@@ -32,6 +32,8 @@ def record_current_answer(answer, current_question_id, session):
     '''
     Validates and stores the answer for the current question to django session.
     '''
+    session[str(current_question_id)+"_response"]=answer.strip()
+
     return True, ""
 
 
@@ -39,8 +41,13 @@ def get_next_question(current_question_id):
     '''
     Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
     '''
-
-    return "dummy question", -1
+    qid = 1
+    if current_question_id :
+       qid =current_question_id+1
+    if qid<len(PYTHON_QUESTION_LIST):
+        return PYTHON_QUESTION_LIST[qid-1]["question_text"]+"\n Options are:"+"\n".join(PYTHON_QUESTION_LIST[qid-1]["options"]), qid
+    else:
+        return None,None
 
 
 def generate_final_response(session):
@@ -48,5 +55,10 @@ def generate_final_response(session):
     Creates a final result message including a score based on the answers
     by the user for questions in the PYTHON_QUESTION_LIST.
     '''
+    score=0
+    for i in range(len(PYTHON_QUESTION_LIST)):
+        if str(i+1)+"_response" in session:
+            if session.get(str(i+1)+"_response") ==  PYTHON_QUESTION_LIST[i]["answer"]:
+                score+=1
 
-    return "dummy result"
+    return "Score is:"+str(score)
